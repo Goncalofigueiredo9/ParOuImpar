5f4e73d31f790b1d4751192f974eec61cf375b67
GoncaloF <GoncaloF@WIN-GESFLHLR4EE>
2019-11-18 16:45:49 +0000

    Adicionar arquivos de projeto.

---
+++ ParOuImpar/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParOuImpar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Par fait = new Par();
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            fait.recebe = textBox1.Text;
+            textBox1.Text = fait.enviar;
+        }
+    }
+}
---
+++ ParOuImpar/Par.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParOuImpar
+{
+    class Par
+    {
+        private int valor;
+        public string recebe
+        {
+            set
+            {
+                valor = Convert.ToInt32(value);
+            }
+        }
+
+        public string enviar
+        {
+            get
+            {
+                if (valor % 2 == 0) return "Par";
+                else return "Ímpar";
+            }         
+        }
+    }
+}
